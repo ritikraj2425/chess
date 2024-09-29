fe57f66e02f7f129e1cabe8515dc20d8d088de3e
Ritik Raj <ritikraj@Ritiks-MacBook-Pro.local>
2024-09-29 19:55:06 +0530

    Resolved merge conflicts

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const WS_URL = "ws://localhost:8080";
+const WS_URL = "ws://localhost:3002";
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
